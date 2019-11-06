1675a4d2eafe2fe04dd6cc4d39e8ddc21624a53f
Thorbjörn Sundragon <puggan@spiro.se>
2019-11-06 23:38:01 +0100

    Converted 403 to pure PHP

--- themes/feidernd/authorize/authorize_403.php
+++ themes/feidernd/authorize/authorize_403.php
@@ -6,38 +6,66 @@
  * @package SimpleSAMLphp
  */
 
+header('HTTP/1.0 403 Forbidden');
+
 if (!array_key_exists('StateId', $_REQUEST)) {
-	throw new SimpleSAML_Error_BadRequest('Missing required StateId query parameter.');
+    throw new SimpleSAML_Error_BadRequest('Missing required StateId query parameter.');
 }
+
 $state = SimpleSAML_Auth_State::loadState($_REQUEST['StateId'], 'authorize:Authorize');
 
 $globalConfig = SimpleSAML_Configuration::getInstance();
 $t = new SimpleSAML_XHTML_Template($globalConfig, 'authorize:authorize_403.php');
 if (isset($state['Source']['auth'])) {
-	$t->data['LogoutURL'] = SimpleSAML\Module::getModuleURL('core/authenticate.php', array('as' => $state['Source']['auth'])) . "&logout";
+    $authUrl = SimpleSAML\Module::getModuleURL(
+        'core/authenticate.php',
+        ['as' => $state['Source']['auth']]
+    );
+    $t->data['LogoutURL'] = "{$authUrl}&logout";
 }
-header('HTTP/1.0 403 Forbidden');
-#$t->show();
 
+//<editor-fold desc="$contactParagraph">
 if (isset($state['SPMetadata'])) {
-	$entity = $state['SPMetadata'];
+    $entity = $state['SPMetadata'];
 } elseif (isset($state['Destination'])) {
-	$entity = $state['Destination'];
+    $entity = $state['Destination'];
 } else {
-	$entity = [];
+    $entity = [];
 }
 
 $foundContact = null;
 if (isset($entity['contacts'])) {
-	foreach ($entity['contacts'] as $contact) {
-		if (in_array($contact['contactType'], ['technical', 'support', 'administrative'])) {
-			$foundContact = $contact;
-			break;
-		}
-	}
+    foreach ($entity['contacts'] as $contact) {
+        if (in_array($contact['contactType'], ['technical', 'support', 'administrative'])) {
+            $foundContact = $contact;
+            break;
+        }
+    }
 }
 
-?>
+$contactParagraph = '<p>Kontakta administratören för tjänsten du försöker komma åt för mer information om detta fel.</p>';
+if (isset($foundContact)) {
+    $contactParagraph = <<<HTML
+    <p>
+        Kontakta
+        <a href="mailto:{$foundContact['emailAddress']}">{$foundContact['emailAddress']}</a>
+        för mer information om detta fel.
+    </p>
+    HTML;
+} elseif (isset($entity['OrganizationURL'])) {
+    $contactParagraph = <<<HTML
+    <p>
+        Kontakta administratören för
+        <a href="{$entity['OrganizationURL']}">{$entity['OrganizationURL']}</a>
+        för mer information om detta fel.
+    </p>
+    HTML;
+}
+//</editor-fold>
+
+$themeUrl = SimpleSAML_Module::getModuleURL('scouterna-theme');
+
+echo <<<HTML
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
 
@@ -46,32 +74,22 @@ if (isset($entity['contacts'])) {
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<link rel='stylesheet' href="<?php echo SimpleSAML_Module::getModuleURL('scouterna-theme/feidernd.css'); ?>" type='text/css' />
+	<link rel='stylesheet' href="{$themeUrl}/feidernd.css" type='text/css' />
 </head>
 
 <body class="login">
 
 	<div id="logo">
-		<img alt="logo" src="<?php echo SimpleSAML_Module::getModuleURL('scouterna-theme/logoscouterna.png') ?>" />
+		<img alt="logo" src="{$themeUrl}/logoscouterna.png" />
 	</div>
 
 	<div id="login">
 		<h2>Du saknar behörighet</h2>
 		<p>Du är inloggad men saknar behörighet för den här tjänsten.</p>
-		<p>
-			Kontakta
-			<?php if (isset($foundContact)) : ?>
-				<a href="<?= $foundContact['emailAddress'] ?>"><?= $foundContact['emailAddress'] ?></a>
-			<?php elseif (isset($entity['OrganizationURL'])) : ?>
-				administratören för
-				<a href="<?= $entity['OrganizationURL'] ?>"><?= $entity['OrganizationURL'] ?></a>
-			<?php else : ?>
-				administratören för tjänsten du försöker komma åt
-			<?php endif; ?>
-			för mer information om detta fel.
-		</p>
+		{$contactParagraph}
 	</div>
 
 </body>
 
 </html>
+HTML;
