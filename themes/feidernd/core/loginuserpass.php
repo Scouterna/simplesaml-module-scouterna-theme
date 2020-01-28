29183eedcd84c5eba4dbcd85746de71141d1991f
Alexander Krantz <klako@hotmail.se>
2020-01-28 19:24:33 +0100

    Changed label, added tooltip.

--- themes/feidernd/core/loginuserpass.php
+++ themes/feidernd/core/loginuserpass.php
@@ -12,16 +12,16 @@ $loginButtonTitle = $this->t('{login:login_button}');
 //<editor-fold desc="$usernameInputValueAttribute">
 $usernameInputValueAttribute = '';
 if (isset($this->data['username'])) {
-    $usernameInputValueAttribute = 'value="' . htmlspecialchars($this->data['username'], ENT_QUOTES) . '"';
+	$usernameInputValueAttribute = 'value="' . htmlspecialchars($this->data['username'], ENT_QUOTES) . '"';
 }
 //</editor-fold>
 
 //<editor-fold desc="$errorDiv">
 $errorDiv = '';
 if (!empty($this->data['errorcode'])) {
-    $errorTitle = $this->t('{errors:title_' . $this->data['errorcode'] . '}');
-    $errorMessage = $this->t('{errors:descr_' . $this->data['errorcode'] . '}');
-    $errorDiv = <<<HTML
+	$errorTitle = $this->t('{errors:title_' . $this->data['errorcode'] . '}');
+	$errorMessage = $this->t('{errors:descr_' . $this->data['errorcode'] . '}');
+	$errorDiv = <<<HTML
     <div id="error">
         <img src="{$this->data['baseurlpath']}resources/icons/experience/gtk-dialog-error.48x48.png" style="float: right; margin: 15px;" alt="Error" />
         <h2>{$errorLabel}</h2>
@@ -35,16 +35,16 @@ if (!empty($this->data['errorcode'])) {
 //<editor-fold desc="$linkUl">
 $linkUl = '';
 if (!empty($this->data['links'])) {
-    $links = [];
-    foreach ($this->data['links'] as $linkData) {
-        $linkUrl = htmlspecialchars($linkData['href'], ENT_QUOTES);
-        $linkText = htmlspecialchars($this->t($linkData['text']), ENT_QUOTES);
-        $links[] = <<<HTML
+	$links = [];
+	foreach ($this->data['links'] as $linkData) {
+		$linkUrl = htmlspecialchars($linkData['href'], ENT_QUOTES);
+		$linkText = htmlspecialchars($this->t($linkData['text']), ENT_QUOTES);
+		$links[] = <<<HTML
             <li><a href="{$linkUrl}">{$linkText}</a></li>
         HTML;
-    }
-    $linkUl = implode(PHP_EOL, $links);
-    $linkUl = str_replace(PHP_EOL, PHP_EOL . $defaultIndent, <<<HTML
+	}
+	$linkUl = implode(PHP_EOL, $links);
+	$linkUl = str_replace(PHP_EOL, PHP_EOL . $defaultIndent, <<<HTML
     <ul class="links" style="margin-top: 2em;">
         {$linkUl}
     </ul>
@@ -55,63 +55,63 @@ if (!empty($this->data['links'])) {
 //<editor-fold desc="$inputsHtml">
 $inputsHtml = '';
 if (!empty($this->data['stateparams'])) {
-    $inputs = [];
-    foreach ($this->data['stateparams'] as $rawName => $rawValue) {
-        $name = htmlspecialchars($rawName, ENT_QUOTES);
-        $value = htmlspecialchars($rawValue, ENT_QUOTES);
-        $inputs[] = <<<HTML
+	$inputs = [];
+	foreach ($this->data['stateparams'] as $rawName => $rawValue) {
+		$name = htmlspecialchars($rawName, ENT_QUOTES);
+		$value = htmlspecialchars($rawValue, ENT_QUOTES);
+		$inputs[] = <<<HTML
         <input type="hidden" name="{$name}" value="{$value}" />
         HTML;
-    }
-    $inputsHtml = implode(PHP_EOL . $defaultIndent, $inputs);
+	}
+	$inputsHtml = implode(PHP_EOL . $defaultIndent, $inputs);
 }
 //</editor-fold>
 
 //<editor-fold desc="$languagebarDiv">
 $languagebarDiv = '';
 if (empty($_POST) && ($this->data['hideLanguageBar'] ?? false) !== true) {
-    $languages = $this->getLanguageList();
-    $langnames = [
-        'no' => 'Bokmål',
-        'nn' => 'Nynorsk',
-        'se' => 'Sámegiella',
-        'sam' => 'Åarjelh-saemien giele',
-        'da' => 'Dansk',
-        'en' => 'English',
-        'de' => 'Deutsch',
-        'sv' => 'Svenska',
-        'fi' => 'Suomeksi',
-        'es' => 'Español',
-        'eu' => 'Euskara',
-        'fr' => 'Français',
-        'nl' => 'Nederlands',
-        'lb' => 'Luxembourgish',
-        'cs' => 'Czech',
-        'sl' => 'Slovenščina', // Slovensk
-        'hr' => 'Hrvatski', // Croatian
-        'hu' => 'Magyar', // Hungarian
-        'pl' => 'Język polski', // Polish
-        'pt' => 'Português', // Portuguese
-        'pt-br' => 'Português brasileiro', // Portuguese
-        'tr' => 'Türkçe',
-    ];
+	$languages = $this->getLanguageList();
+	$langnames = [
+		'no' => 'Bokmål',
+		'nn' => 'Nynorsk',
+		'se' => 'Sámegiella',
+		'sam' => 'Åarjelh-saemien giele',
+		'da' => 'Dansk',
+		'en' => 'English',
+		'de' => 'Deutsch',
+		'sv' => 'Svenska',
+		'fi' => 'Suomeksi',
+		'es' => 'Español',
+		'eu' => 'Euskara',
+		'fr' => 'Français',
+		'nl' => 'Nederlands',
+		'lb' => 'Luxembourgish',
+		'cs' => 'Czech',
+		'sl' => 'Slovenščina', // Slovensk
+		'hr' => 'Hrvatski', // Croatian
+		'hu' => 'Magyar', // Hungarian
+		'pl' => 'Język polski', // Polish
+		'pt' => 'Português', // Portuguese
+		'pt-br' => 'Português brasileiro', // Portuguese
+		'tr' => 'Türkçe',
+	];
 
-    $links = [];
-    foreach ($languages as $lang => $current) {
-        if ($current) {
-            $links[] = $langnames[$lang];
-            continue;
-        }
+	$links = [];
+	foreach ($languages as $lang => $current) {
+		if ($current) {
+			$links[] = $langnames[$lang];
+			continue;
+		}
 
-        $rawUrl = SimpleSAML_Utilities::addURLparameter(SimpleSAML_Utilities::selfURL(), ['language' => $lang]);
-        $linkUrl = htmlspecialchars($rawUrl, ENT_QUOTES);
+		$rawUrl = SimpleSAML_Utilities::addURLparameter(SimpleSAML_Utilities::selfURL(), ['language' => $lang]);
+		$linkUrl = htmlspecialchars($rawUrl, ENT_QUOTES);
 
-        $links[] = <<<HTML
+		$links[] = <<<HTML
         <a href="{$linkUrl}">{$langnames[$lang]}</a>
         HTML;
-    }
-    $languagebarDiv = implode(' | ', $links);
-    $languagebarDiv = <<<HTML
+	}
+	$languagebarDiv = implode(' | ', $links);
+	$languagebarDiv = <<<HTML
     <div id="languagebar">{$languagebarDiv}</div>
     HTML;
 }
@@ -151,16 +151,15 @@ echo <<<HTML
 	        <form name="loginform" id="loginform" action="?" method="post">
                 <h2>Inloggning</h2>
                 <p>
-                    <label>
-                        Medlemsnummer<br />
-                        <input type="text" name="username" id="username" class="input" {$usernameInputValueAttribute} size="20" tabindex="10" />
-                    </label>
+					<label>Användarnamn</label>
+					<span class="tooltipqmark" data-tooltip="Medlemsnummer, personnummer, eller primär epostadress">
+						<img src="{$themeUrl}/questionmark.png" />
+					</span><br/>
+					<input type="text" name="username" id="username" class="input" {$usernameInputValueAttribute} size="20" tabindex="10" />
                 </p>
                 <p>
-                    <label>
-                        {$passwordLabel}<br />
-                        <input type="password" name="password" id="user_pass" class="input" value="" size="20" tabindex="20" />
-                    </label>
+                    <label>{$passwordLabel}</label><br/>
+					<input type="password" name="password" id="user_pass" class="input" value="" size="20" tabindex="20" />
                 </p>
                 <p>
                     <label>
